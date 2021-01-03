b4d29c63aad6208e0931680e391e4b071d47c410
MBronko <michal.brono@protonmail.com>
2021-01-03 21:20:32 +0100

    Add primitive menu

--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
-#define _GNU_SOURCE
-
+#include <stdlib.h>
 #include <ncurses.h>
 #include <string.h>
 #include <ctype.h>
@@ -22,9 +20,9 @@ int n_choices = sizeof(choices) / sizeof(char *);
 
 void draw_menu();
 
-void menu();
+int menu(int game_active);
 
-void game();
+int game();
 
 int main() {
     setlocale(LC_ALL, "");
@@ -38,8 +36,8 @@ int main() {
     init_pair(MOUNTAIN_PAIR, COLOR_BLUE, COLOR_WHITE);
     init_pair(PLAYER_PAIR, COLOR_RED, COLOR_MAGENTA);
 
-//    menu();
-    game();
+//    game();
+    menu(0);
 
     clear();
     addstr("press any key to exit...");
@@ -48,37 +46,84 @@ int main() {
     return 0;
 }
 
-//void draw_menu(int highlight) {
-//    int x, y, i;
-//    x = 2;
-//    y = 2;
-//    box(stdscr, 0, 0);
-//    for (i = 0; i < n_choices; ++i) {
-//        if (highlight == i + 1) /* High light the present choice */
-//        {
-//            wattron(stdscr, A_REVERSE);
-//            mvwprintw(stdscr, y, x, "%s", choices[i]);
-//            wattroff(stdscr, A_REVERSE);
-//        } else
-//            mvwprintw(stdscr, y, x, "%s", choices[i]);
-//        ++y;
-//    }
-////    wrefresh(stdscr);
-//    refresh();
-//}
-//
-//void menu() {
-//    draw_menu(1);
-//    getchar();
-////    game();
-//}
+void draw_menu(int highlight, int game_active) {
+    int x, y, i;
+    x = 2;
+    y = 2;
+    box(stdscr, 0, 0);
+    for (i = 1; i <= n_choices; ++i) {
+        if(!game_active && i == 1){
+            attron(A_DIM);
+            mvwprintw(stdscr, y, x, "%s", choices[i-1]);
+            attroff(A_DIM);
 
-void game() {
+        }
+        else if (highlight == i)
+        {
+            attron(A_REVERSE);
+            mvwprintw(stdscr, y, x, "%s", choices[i-1]);
+            attroff(A_REVERSE);
+        } else
+            mvwprintw(stdscr, y, x, "%s", choices[i-1]);
+        ++y;
+    }
+//    wrefresh(stdscr);
+    refresh();
+}
+
+int menu_option(int option) {
+    switch (option) {
+        case 1:
+            return 0;
+        case 2:
+            return 1;
+        case 3:
+            endwin();
+            exit(0);
+        default:
+            return 0;
+    }
+}
+
+int menu(int game_active) {
+    int highlight = game_active ? 1 : 2;
+    int new_game, c;
+    draw_menu(highlight, game_active);
+    while ((c = getch()) != 'q') {
+        switch (tolower(c)) {
+            case KEY_UP:
+            case 'w':
+                if ((game_active && highlight != 1) || (!game_active && highlight != 2)) highlight--;
+                break;
+            case KEY_DOWN:
+            case 's':
+                if (highlight != n_choices) highlight++;
+                break;
+            case 10:
+                if (menu_option(highlight)) {
+                    if (game_active) return 1;
+                    do { new_game = game(); } while (new_game);
+                } else {
+                    return 0;
+                }
+            default:
+                continue;
+        }
+        draw_menu(highlight, game_active);
+    }
+    getchar();
+    return 0;
+}
+
+int game() {
 //    int game_x = 10, game_y = 20;
+    int colors[10] = {COLOR_BLUE, COLOR_CYAN, COLOR_MAGENTA, COLOR_MAGENTA, COLOR_YELLOW};
+    int n_colors = 4;
     int c, x = 20, y = 10;
     int counter = 0;
+    clear();
     while ((c = getch()) != 'q') {
-        switch (c) {
+        switch (tolower(c)) {
             case ' ':
                 y -= 2;
                 break;
@@ -88,7 +133,7 @@ void game() {
                 break;
             case KEY_LEFT:
             case 'a':
-                x--;
+                x -= 2;
                 break;
             case KEY_DOWN:
             case 's':
@@ -96,16 +141,21 @@ void game() {
                 break;
             case KEY_RIGHT:
             case 'd':
-                x++;
+                x += 2;
+                break;
+            case 'p':
+                if (menu(1)) return 1;
                 break;
             default:
                 continue;
         }
-        clear();
+//        clear();
         counter++;
-        attron(COLOR_PAIR(counter % 4 + 1));
+        attron(COLOR_PAIR(counter % n_colors + 1));
         mvwprintw(stdscr, y, x, "\xe2\x96\x88");
-        attroff(COLOR_PAIR(counter % 4 + 1));
+        mvwprintw(stdscr, y, x + 1, "\xe2\x96\x88");
+        attroff(COLOR_PAIR(counter % n_colors + 1));
         refresh();
     }
+    return 0;
 }
